f815f472344c561322d27f7202aaf69069b90d1f
Jessie Barré <jessie.barre@renault.com>
2020-05-28 14:48:11 +0200

    fix Dto preparation

--- server/src/main/java/com/network/server/dtos/UserDto.java
+++ server/src/main/java/com/network/server/dtos/UserDto.java
@@ -1,15 +1,18 @@
 package com.network.server.dtos;
 
+import java.time.LocalDate;
+
 public class UserDto {
 
     private String firstName;
 
     private String lastName;
 
-    // TODO = age
+    private LocalDate birthDate;
 
-    public UserDto(String firstName, String lastName) {
+    public UserDto(String firstName, String lastName, LocalDate birthDate ) {
         this.firstName = firstName;
         this.lastName = lastName;
+        this.birthDate = birthDate;
     }
 }
